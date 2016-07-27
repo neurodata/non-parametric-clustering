9f3f7e627b3ddfa00783438439df68d388e48eee
guisf <guifranca@gmail.com>
2016-07-27 01:40:21 -0400

    bla

--- gaussian_mle.py
+++ gaussian_mle.py
@@ -7,6 +7,7 @@ from __future__ import division
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 
 
 def gaussian_mle_estimator(x):
@@ -18,26 +19,16 @@ def gaussian_mle_estimator(x):
     mean_mle = np.array([x[:, i].mean() for i in range(D)])
     cov_mle = np.array(sum([np.outer(x[i] - mean_mle, x[i] - mean_mle) 
                         for i in range(len(x))]))
-    cov_mle = cov_mle / (len(x) - 1)
+    cov_mle = cov_mle/(len(x)-1)
     return mean_mle, cov_mle
-
-def gaussianD(x, mu, sigma):
-    D = len(x)
-    c = np.sqrt(((2*np.pi)**D)*np.linalg.det(sigma))
-    c = 1/c
-    inv_sigma = np.linalg.inv(sigma)
-    return c*np.exp(-(1/2)*(x-mu).dot(inv_sigma.dot(x-mu)))
-
-def gaussian1D(x, mu, sigma):
-    c = np.sqrt(2*np.pi)*sigma
-    c = 1/c
-    return c*np.exp(-(1/2)*((x-mu)**2)/(sigma**2))
-
 
 def histogram(x, mean_mle, sigma_mle):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     colors = ['b', 'r', 'g']
+
+    
+
     for i in range(len(mean_mle)):
         ax.hist(x[:,i], color=colors[i], bins=80, normed=True, alpha=.3)
         xs = np.linspace(x[:,i].min(), x[:,i].max(), 200)
--- gmm.py
+++ gmm.py
@@ -1,13 +1,76 @@
 """
 EM algorithm for GMMM
 
+1. Unitialize \mu_k, \Sigma_k, and \pi_k. Evaluate log likelihood
+2. E Step: evaluate responsabilities using the current values of parameters.
+3. M Step: re-estimate \mu_k, \Sigma_k, and \pi_k using current
+   responsabilities.
+4. Evaluate log likelihood and check for convergence. Repeat from step 2
+   if not converged.
+
+
 """
 
 from __future__ import division
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
 
 
+def gaussian(x, mu, sigma):
+    """Return multidimensional gaussian at point x."""
+    return multivariate_normal.pdf(x, mu, sigma)
+
+def log_likelihood(x, mu, sigma, pi):
+    """Return the log likelihood function based on data and current
+    parameters.
+    
+    """
+    K = len(pi)
+    log_lh = 0
+    for xn in x:
+        log_lh += np.array([pi[k]*gaussian(xn, mu[k], sigma[k]) 
+                            for k in range(K)]).sum()
+    return log_lh
+
+def gmm_mle(x, mu, sigma, pi, tol=0.001):
+    """Estimate a GMM using maximum likelihood estimation through EM
+    algorithm. mu, sigma, pi are the initial values.
+    TODO: vectorize things.
+    
+    """
+    K = len(pi)
+    N = len(x)
+    converged = False
+    gamma = np.zeros((N, K), dtype=float)
+    old_log_lh = log_likelihood(x, mu, sigma, pi)
+
+    while not converged:
+        
+        # compute responsabilities, E-step
+        for n in range(N):
+            total = sum([pi[j]*gaussian(x[n], mu[j], sigma[j]) 
+                            for j in range(K)])
+            for k in range(K):
+                gamma[n, k] = pi[k]*gaussian(x[n], mu[k], sigma[k])/sum_piN
+
+        # compute parameters, M-step
+        for k in range(K):
+            Nk = gamma[:,k].sum()
+            mu[k] = sum([gamma[n,k]*x[n]  for n in range(N)])/Nk
+            # TODO: check gaussian collapse under a point
+            sigma[k] = sum([gamma[n,k]*(x[n]-mu[k]).outer(x[n]-mu[k]) 
+                            for n in range(N)])/Nk
+            pi[k] = Nk/N
+
+        # compute log likelihood and check convergence
+        new_log_lh = log_likelihood(x, K, mu, sigma, pi)
+        if abs(new_log_lh - old_log_lh) <= tol:
+            converged = True
+        else:
+            old_log_lh = new_log_lh
+
+    return mu, sigma, pi
 
 
